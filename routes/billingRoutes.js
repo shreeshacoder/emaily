4ff74f6d04c22bb51a892007f23f3263404180a3
BlacknMellow <shreesha.addala2012@vit.ac.in>
2018-09-23 18:39:25 -0400

    Added billing

---
+++ routes/billingRoutes.js
@@ -0,0 +1,16 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
+module.exports = app => {
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      source: req.body.id, // obtained with Stripe.js
+      description: "Charge Emaily"
+    });
+    req.user.credits += 5;
+    const user = req.user.save();
+    res.send(user);
+  });
+};
--- services/passport.js
+++ services/passport.js
@@ -23,17 +23,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //User exists in DB
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        //User exists in DB
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
