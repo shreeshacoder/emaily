896f513a12333a7f8b6d0ce52c147f58cfeb7e86
BlacknMellow <shreesha.addala2012@vit.ac.in>
2018-09-23 20:16:31 -0400

    Production fixes

--- index.js
+++ index.js
@@ -25,16 +25,5 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  // For files like main.js
-  app.use(express.static("client/build"));
-
-  // For routes handled by React-Router
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
